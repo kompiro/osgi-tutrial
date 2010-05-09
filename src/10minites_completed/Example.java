d850895a7c952516174b395ce259d13e1d13115c
Hiroki Kondo <kompiro@gmail.com>
2010-05-10 00:05:44 +0900

    更新分を追加

---
+++ src/10minites_completed/Example.java
@@ -0,0 +1,1 @@
+package org.foo.example;import org.osgi.framework.BundleActivator;import org.osgi.framework.BundleContext;public class Example implements BundleActivator {    public void start(BundleContext bc) {        System.out.println("Hello world");    }    public void stop(BundleContext bc) {        System.out.println("Goodbye world");      }}
